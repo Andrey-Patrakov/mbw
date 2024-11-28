017138944f247adc973250ba9d2e8ff814034ad6
Patrakov Andrey Vladimirovich <a.v.patrakov95@gmail.com>
2024-11-28 20:52:30 +0300

    chore(ml): add bias to nn linear layer

--- ml/nn/layers.py
+++ ml/nn/layers.py
@@ -8,11 +8,13 @@ class Linear:
         self._out_features = out_features
 
         self._w = 2 * np.random.random((in_features, out_features)) - 1
+        self._b = 2 * np.random.random((1, out_features)) - 1
 
     def forward(self, x):
         self._cache = x
-        return x @ self._w
+        return x @ self._w + self._b
 
     def backward(self, error, lr):
-        self._w += self._cache.T @ error * lr
+        self._w += (self._cache.T @ error) * lr
+        self._b += np.sum(error, axis=0, keepdims=True) * lr
         return error @ self._w.T
